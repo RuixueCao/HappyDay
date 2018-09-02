5bc73c1a89aa66f85d8d2456a604ff37a2cacd4f
RuixueCao <976307757@qq.com>
2018-09-02 13:24:33 +0800

    很迷

--- server/server_dev.js
+++ server/server_dev.js
@@ -2,18 +2,63 @@ var http = require("http");
 var express = require("express");
 var fs = require("fs");
 var url = require("url");
+var querystring = require('querystring');
+var ejs = require('ejs');
+
 
 http.createServer(function (request,response) {
+    var up = url.parse(request.url);
+    var pathname = up.pathname;
 
-    var pathname = url.parse(request.url).pathname;
+    var data = '';
+    if(pathname=='/login'){
+        var retData = {
+            isSuccess:false,
+            message:'',
+            content:null
+        }
+        request.on("data",function (chunk) {
+            data += chunk;
+        })
+        request.on("end",function () {
+            var username = JSON.parse(data).username;
+            var password = JSON.parse(data).password;
 
-    var data;
-    if(pathname=='/main'){
-        data = fs.readFileSync('../app/main.html');
+            if(username=="wang7liang" && password=="123456"){
+                retData.isSuccess=true;
+                retData.content={username:username,password:password};
+            }else{
+                retData.isSuccess=false;
+                retData.message="用户名或者密码错误";
+            }
+            response.writeHead(200, {'Content-Type': 'text/plain'});
+            response.end(JSON.stringify(retData));
+        })
+    }else if(pathname=='/main'){
+
+        data = fs.readFileSync('../app/main.html').toString();
+        var username = querystring.parse(up.query).username;
+        var password = querystring.parse(up.query).password;
+
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
+
+    }else if(pathname=='/detail'){
+        var name = querystring.parse(up.query).name
+        var sex = querystring.parse(up.query).sex
+        data = fs.readFileSync('../app/detail.ejs').toString();
+        ejs.renderFile('../app/detail.ejs',{name:name,sex:sex},function (err,str) {
+            data = str;
+        })
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
     }else{
-        data = fs.readFileSync('../app/login.html');
+        data = fs.readFileSync('../app/login.html').toString();
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write(data);
+        response.end();
     }
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.write(data.toString())
-    response.end();
+
 }).listen(80);
